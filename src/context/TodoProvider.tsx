fa1fb8ca85726ad7af05284d00699b15b43d979a
Arinze Ogbuniba <niklaus.oa@gmail.com>
2025-10-06 19:13:17 +0100

    feat: store and retrieve tasks to localStorage

--- src/context/TodoProvider.tsx
+++ src/context/TodoProvider.tsx
@@ -1,10 +1,23 @@
 import type { Todos } from "@/types/myTypes";
-import { useReducer, type ReactNode } from "react";
+import { useEffect, useReducer, type ReactNode } from "react";
 import { TodoContext } from "./TodoContext";
 import { cartReducer } from "./cartReducer";
 
 export default function TodoProvider({ children }: { children: ReactNode }) {
-  const [todos, dispatch] = useReducer(cartReducer, []);
+  const initializer = (initialTodos: Todos[]) => {
+    try {
+      const storedTodos = localStorage.getItem("todos");
+      return storedTodos ? JSON.parse(storedTodos) : initialTodos;
+    } catch (error) {
+      return initialTodos;
+    }
+  };
+
+  const [todos, dispatch] = useReducer(cartReducer, [], initializer);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return <TodoContext value={{ todos, dispatch }}>{children}</TodoContext>;
 }
